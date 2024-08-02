217bf34eccc753c58ac89589ecee9c96b4bf6908
phananhlocpal <anhloc280@gmail.com>
2024-08-02 08:39:55 +0700

    add products

---
+++ MVC21BITV01Test/Controllers/SanPhamsController.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVC21BITV01Test.Models;
+using System.IO;
+
+namespace MVC21BITV01Test.Controllers
+{
+    public class SanPhamsController : Controller
+    {
+        private readonly QlbanHangContext _context;
+
+        public SanPhamsController(QlbanHangContext context)
+        {
+            _context = context;
+        }
+
+        // GET: SanPhams
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.SanPhams.ToListAsync());
+        }
+
+        // GET: SanPhams/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sanPham = await _context.SanPhams
+                .FirstOrDefaultAsync(m => m.MaSp == id);
+            if (sanPham == null)
+            {
+                return NotFound();
+            }
+
+            return View(sanPham);
+        }
+
+        // GET: SanPhams/Create
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SanPhams/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaSp,TenSp,DonViTinh,DonGia")] SanPham sanPham, IFormFile Hinh)
+        {
+            if (Hinh != null && Hinh.Length > 0)
+            {
+                var supportedTypes = new[] { "jpg", "jpeg", "png", "gif" };
+                var fileExt = Path.GetExtension(Hinh.FileName).Substring(1).ToLower();
+
+                if (!supportedTypes.Contains(fileExt))
+                {
+                    ModelState.AddModelError("Hinh", "Invalid image format. Only JPG, JPEG, PNG, and GIF are allowed.");
+                }
+                else
+                {
+                    // Save the image to wwwroot/img_products
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img_products", Hinh.FileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await Hinh.CopyToAsync(stream);
+                    }
+
+                    sanPham.Hinh = Hinh.FileName;
+                }
+            }
+
+            if (sanPham.DonGia <= 0)
+            {
+                ModelState.AddModelError("DonGia", "DonGia must be a number greater than 0.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(sanPham);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(sanPham);
+        }
+
+        // GET: SanPhams/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sanPham = await _context.SanPhams.FindAsync(id);
+            if (sanPham == null)
+            {
+                return NotFound();
+            }
+            return View(sanPham);
+        }
+
+        // POST: SanPhams/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MaSp,TenSp,DonViTinh,DonGia,Hinh")] SanPham sanPham)
+        {
+            if (id != sanPham.MaSp)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(sanPham);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SanPhamExists(sanPham.MaSp))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(sanPham);
+        }
+
+        // GET: SanPhams/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var sanPham = await _context.SanPhams
+                .FirstOrDefaultAsync(m => m.MaSp == id);
+            if (sanPham == null)
+            {
+                return NotFound();
+            }
+
+            return View(sanPham);
+        }
+
+        // POST: SanPhams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var sanPham = await _context.SanPhams.FindAsync(id);
+            if (sanPham != null)
+            {
+                _context.SanPhams.Remove(sanPham);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SanPhamExists(int id)
+        {
+            return _context.SanPhams.Any(e => e.MaSp == id);
+        }
+    }
+}
---
+++ MVC21BITV01Test/Models/SanPhamCreateDto.cs
@@ -0,0 +1,17 @@
+﻿namespace MVC21BITV01Test.Models
+{
+    public class SanPhamCreateDto
+    {
+        public int MaSp { get; set; }
+
+        public string TenSp { get; set; } = null!;
+
+        public string? DonViTinh { get; set; }
+
+        public double? DonGia { get; set; }
+
+        public byte[]? Hinh { get; set; }
+
+        public virtual ICollection<ChiTietHoaDon> ChiTietHoaDons { get; set; } = new List<ChiTietHoaDon>();
+    }
+}
