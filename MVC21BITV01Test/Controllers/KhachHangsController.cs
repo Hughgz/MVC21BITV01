ca7831af0062a5340b03dc465e152ec73e9976ba
Hughgz <minhhieu.swe@gmail.com>
2024-08-02 08:36:08 +0700

    add customer

---
+++ MVC21BITV01Test/Controllers/KhachHangsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVC21BITV01Test.Models;
+
+namespace MVC21BITV01Test.Controllers
+{
+    public class KhachHangsController : Controller
+    {
+        private readonly QlbanHangContext _context;
+
+        public KhachHangsController(QlbanHangContext context)
+        {
+            _context = context;
+        }
+
+        // GET: KhachHangs
+        public async Task<IActionResult> Index()
+        {
+            var qlbanHangContext = _context.KhachHangs.Include(k => k.ThanhPhoNavigation);
+            return View(await qlbanHangContext.ToListAsync());
+        }
+
+        // GET: KhachHangs/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var khachHang = await _context.KhachHangs
+                .Include(k => k.ThanhPhoNavigation)
+                .FirstOrDefaultAsync(m => m.MaKh == id);
+            if (khachHang == null)
+            {
+                return NotFound();
+            }
+
+            return View(khachHang);
+        }
+
+        // GET: KhachHangs/Create
+        public IActionResult Create()
+        {
+            ViewData["ThanhPho"] = new SelectList(_context.ThanhPhos, "MaThanhPho", "MaThanhPho");
+            return View();
+        }
+
+        // POST: KhachHangs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(KhachHang khachHang)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _context.Add(khachHang);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                ViewData["ThanhPho"] = new SelectList(_context.ThanhPhos, "MaThanhPho", "MaThanhPho", khachHang.ThanhPho);
+                return View(khachHang);
+            }
+            catch (Exception ex) { 
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // GET: KhachHangs/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var khachHang = await _context.KhachHangs.FindAsync(id);
+            if (khachHang == null)
+            {
+                return NotFound();
+            }
+            ViewData["ThanhPho"] = new SelectList(_context.ThanhPhos, "MaThanhPho", "MaThanhPho", khachHang.ThanhPho);
+            return View(khachHang);
+        }
+
+        // POST: KhachHangs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("MaKh,TenCty,DiaChi,DienThoai,ThanhPho")] KhachHang khachHang)
+        {
+            if (id != khachHang.MaKh)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(khachHang);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!KhachHangExists(khachHang.MaKh))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ThanhPho"] = new SelectList(_context.ThanhPhos, "MaThanhPho", "MaThanhPho", khachHang.ThanhPho);
+            return View(khachHang);
+        }
+
+        // GET: KhachHangs/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var khachHang = await _context.KhachHangs
+                .Include(k => k.ThanhPhoNavigation)
+                .FirstOrDefaultAsync(m => m.MaKh == id);
+            if (khachHang == null)
+            {
+                return NotFound();
+            }
+
+            return View(khachHang);
+        }
+
+        // POST: KhachHangs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var khachHang = await _context.KhachHangs.FindAsync(id);
+            if (khachHang != null)
+            {
+                _context.KhachHangs.Remove(khachHang);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool KhachHangExists(string id)
+        {
+            return _context.KhachHangs.Any(e => e.MaKh == id);
+        }
+    }
+}
